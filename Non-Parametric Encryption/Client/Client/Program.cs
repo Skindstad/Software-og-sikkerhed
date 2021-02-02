dd15ba3a46ff1e482731a0acef74d1eeabd2ac18
Skindstad <39044695+Skindstad@users.noreply.github.com>
2021-02-02 10:54:03 +0100

    Server og client
    Now jeg har lavet en server og en klient der kan tage imod og modtage bekeder

---
+++ Non-Parametric Encryption/Client/Client/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Client();
+        }
+
+        public void Client()
+        {
+            TcpClient client = new TcpClient();
+
+            int port = 13356;
+            IPAddress ip = IPAddress.
+                Parse("192.168.1.22");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+                //send a message
+                Console.Write("Write your message here: ");
+                string text = Console.ReadLine();
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+                Encryption(text, bytes);
+
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            //client.Close();
+        }
+
+
+
+        public void Encryption(string e, byte[] m)
+        {
+
+            for (int i = 0; i < m.Length; i++)
+            {
+                m[i] += 3;
+            }
+
+            e = Encoding.UTF8.GetString(m);
+            Console.WriteLine(e);
+
+        }
+
+        public async void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] bytes = new byte[252];
+            bool isRunning = true;
+            while (isRunning)
+            {
+               int Read = await stream.ReadAsync(bytes, 0, bytes.Length);
+                string text = Encoding.UTF8.GetString(bytes, 0, Read);
+
+                Decryptions(text, bytes);
+            }
+        }
+
+
+        public void Decryptions(string e, byte[] m)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(e);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] -= 3;
+            }
+
+            e = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ Non-Parametric Encryption/Server/Server/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            p.MyServer();
+
+        }
+        public List<TcpClient> clients = new List<TcpClient>();
+        public void MyServer()
+        {
+            IPAddress ip = IPAddress.Parse("192.168.1.22");
+            int port = 13356;
+            TcpListener listener = new TcpListener(ip, port);
+            listener.Start();
+
+            AcceptClients(listener);
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.Write("Write your message here: ");
+                string text = Console.ReadLine();
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+                Encryption(text, bytes);
+
+                //stream.Write(buffer, 0, buffer.Length);
+                foreach (TcpClient client in clients)
+                {
+                    client.GetStream().Write(bytes, 0, bytes.Length);
+                }
+            }
+        }
+
+        public void Encryption(string e, byte[] m)
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                m[i] += 3;
+            }
+
+            e = Encoding.UTF8.GetString(m);
+            Console.WriteLine(e);
+
+        }
+        public async void AcceptClients(TcpListener listener)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                clients.Add(client);
+                NetworkStream stream = client.GetStream();
+                ReceiveMessage(stream);
+            }
+        }
+        public async void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] bytes = new byte[252];
+            bool isRunning = true;
+            while (isRunning)
+            {
+                int Read = await stream.ReadAsync(bytes, 0, bytes.Length);
+                string text = Encoding.UTF8.GetString(bytes, 0, Read);
+                Decryptions(text, bytes);
+
+
+            }
+        }
+
+
+        public void Decryptions(string e, byte[] m)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(e);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] -= 3;
+            }
+
+            e = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(e);
+        }
+    }
+}
