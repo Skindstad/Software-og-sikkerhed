f97afc3b96fb26807940b13da9dc998335854ab8
Skindstad <39044695+Skindstad@users.noreply.github.com>
2021-02-02 09:28:02 +0100

    de er i her deres metode

--- Non-Parametric Encryption/Non-Parametric Encryption/Program.cs
+++ Non-Parametric Encryption/Non-Parametric Encryption/Program.cs
@@ -13,84 +13,43 @@ namespace Non_Parametric_Encryption
        public static void Main(string[] args)
         {
             Program p = new Program();
+            Console.WriteLine("Please write something: ");
+            string message = Console.ReadLine();
 
-            string message = "Hej med dig";
-            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            p.Encryption(message);
+
+            Console.ReadKey();
+
+        }
+
+        public void Encryption(string e)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(e);
 
             for (int i = 0; i < bytes.Length; i++)
             {
                 bytes[i] += 3;
             }
 
-            message = Encoding.UTF8.GetString(bytes);
-            Console.WriteLine(message);
+            e = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(e);
+
+            Decryptions(e);
+
+        }
+
+        public void Decryptions(string e)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(e);
 
             for (int i = 0; i < bytes.Length; i++)
             {
                 bytes[i] -= 3;
             }
 
-            message = Encoding.UTF8.GetString(bytes);
-            Console.WriteLine(message);
-
-            p.MyServer();
+            e = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(e);
         }
 
-        public List<TcpClient> clients = new List<TcpClient>();
-        public void MyServer()
-        {
-            IPAddress ip = IPAddress.Parse("192.168.1.22");
-            int port = 13356;
-            TcpListener listener = new TcpListener(ip, port);
-            listener.Start();
-
-            AcceptClients(listener);
-
-            bool isRunning = true;
-            while (isRunning)
-            {
-                Console.Write("Write your message here: ");
-                string text = Console.ReadLine();
-                byte[] buffer = Encoding.UTF8.GetBytes(text);
-                for (int i = 0; i < buffer.Length; i++)
-                {
-                    buffer[i] += 3;
-
-                }
-                    //stream.Write(buffer, 0, buffer.Length);
-                    foreach (TcpClient client in clients)
-                    {
-                        client.GetStream().Write(buffer, 0, buffer.Length);
-                    }
-                
-            }
-        }
-        public async void AcceptClients(TcpListener listener)
-        {
-            bool isRunning = true;
-            while (isRunning)
-            {
-                TcpClient client = await listener.AcceptTcpClientAsync();
-                clients.Add(client);
-                NetworkStream stream = client.GetStream();
-                ReceiveMessage(stream);
-            }
-        }
-        public async void ReceiveMessage(NetworkStream stream)
-        {
-            byte[] buffer = new byte[256];
-            bool isRunning = true;
-            while (isRunning)
-            {
-                int Read = await stream.ReadAsync(buffer, 0, buffer.Length);
-                string text = Encoding.UTF8.GetString(buffer, 0, Read);
-                for (int i = 0; i < buffer.Length; i++)
-                {
-                    buffer[i] -= 3;
-                }
-                text = Encoding.UTF8.GetString(buffer);
-                Console.Write("client writes: " + text);
-            }
-        }
     }
 }
