52e9a5cec338775fad4f2c6d0a4742fd0d5fa771
Jakob Skindstad Frederiksen <JSFr.skp@edu.mercantec.dk>
2021-02-03 19:33:55 +0100

    working on done

---
+++ Done/Server/Server/cipher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class cipher
+    {
+
+        public readonly byte[] publicKey = new byte[32];
+
+        public static char cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+
+
+        }
+        public static string Encipher(string text, int key)
+            {
+            string output = string.Empty;
+
+            foreach (char ch in text)
+                output += cipher(ch, key);
+
+            return output;
+        }
+        public static string Decipher(string text, int key)
+            {
+            
+            return Encipher(text, 26 - key);
+        }
+
+
+
+
+    }
+}
